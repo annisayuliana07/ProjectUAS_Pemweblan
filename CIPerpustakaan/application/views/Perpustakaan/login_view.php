34ee642a4ed312083fca55072c35bab241741824
Annisa Yuliana Rahma P <annisayulianarp@student.uns.ac.id>
2020-06-02 00:44:13 +0700

    Create login_view.php

---
+++ CIPerpustakaan/application/views/Perpustakaan/login_view.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="<?= base_url('assets_log/'); ?>images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets_log/'); ?>css/main.css">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-form-title" style="background-image: url(assets_log/images/bg-01.jpg);">
+                    <span class="login100-form-title-1">
+                        Welcome Admin!
+                    </span>
+                </div>
+
+                <form class="login100-form validate-form" action="<?php echo base_url('Login/cek_login') ?>" method="post">
+                    <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+                        <span class="label-input100">Username</span>
+                        <input class="input100" type="text" name="username" placeholder="Enter username">
+                        <span class="focus-input100"></span>
+                        <p><?php echo form_error('username'); ?></p>
+                    </div>
+
+                    <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
+                        <span class="label-input100">Password</span>
+                        <input class="input100" type="password" name="pass" placeholder="Enter password">
+                        <span class="focus-input100"></span>
+                        <p><?php echo form_error('password'); ?></p>
+                    </div>
+
+                    <div class="container-login100-form-btn mx-4">
+                        <button type="submit" class="login100-form-btn mx-5">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>vendor/bootstrap/js/popper.js"></script>
+    <script src="<?= base_url('assets_log/'); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>vendor/daterangepicker/moment.min.js"></script>
+    <script src="<?= base_url('assets_log/'); ?>vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets_log/'); ?>js/main.js"></script>
+
+</body>
+
+</html>
